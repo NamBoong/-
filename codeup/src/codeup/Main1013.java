e8ce446115a5fc6169ea90016196bde2295bb02f
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-07-31 00:49:11 +0900

    코드업 1013번 풀이

---
+++ codeup/src/codeup/Main1013.java
@@ -0,0 +1,26 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Main1013 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner (System.in);
+		
+		int value1 = scan.nextInt();
+		int value2 = scan.nextInt();
+		//정수 int 2개를 원했으니 2개를 써주자.
+		
+		
+		scan.close();
+		
+		System.out.println(value1);
+		System.out.println(value2);
+		// 파이썬과 다르게 한 줄에 한개만 값을 입력할 수 있는 것 같다.
+		// 1010번과 크게 다르지않으니 감이 잘 오지않는다면 1010번을 참조하자.
+		
+		
+	}
+
+}
