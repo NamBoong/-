efd35f44a694f151426c8afc0bc8db5fa686c87b
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-07-30 11:27:45 +0900

    코드업 1008번 풀이

---
+++ codeup/src/codeup/Main1008.java
@@ -0,0 +1,12 @@
+package codeup;
+
+public class Main1008 {
+
+	public static void main(String[] args) {
+
+		System.out.println("\u250C\u252C\u2510");
+		System.out.println("\u251c\u253c\u2524");
+		System.out.println("\u2514\u2534\u2518");
+	}
+
+}
