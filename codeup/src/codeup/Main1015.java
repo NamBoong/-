3fbfbdb15a19e11728f4429a0b9c4e5ea474f7cf
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-02 15:31:46 +0900

    코드업 1015번 풀이

---
+++ codeup/src/codeup/Main1015.java
@@ -0,0 +1,20 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Main1015 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		float value1 = scan.nextFloat();
+		
+		System.out.printf(String.format("%.2f",value1));
+		
+		scan.close();
+				
+		
+	}
+
+}
