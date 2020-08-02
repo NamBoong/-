d97a9aa7ec862a3077233f71a4d9e897bf3f669b
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-02 16:03:21 +0900

    코드업 1018번 풀이

---
+++ codeup/src/codeup/Main1018.java
@@ -0,0 +1,22 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Main1018 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		String value1 = scan.next();
+		// 입력을 3:16 이렇게 한다고했으니 이걸 문자로 그대로 표기해보려고했다.
+		//그러니 그냥 그대로 받으면 끝
+		
+		System.out.println(value1);
+		
+		scan.close();
+		
+		
+	}
+
+}
