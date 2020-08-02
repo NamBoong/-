996c3b8739333cbb53bbdc83d84157df8616b942
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-02 16:47:24 +0900

    코드업 1019번 풀이

--- codeup/src/codeup/Main1019.java
+++ codeup/src/codeup/Main1019.java
@@ -17,6 +17,8 @@ public class Main1019 {
 		int day = Integer.valueOf(arr[2]);
 		
 		System.out.printf("%04d.%02d.%02d",year,month,day);
+		//0000.00.00 형식으로 나와야한다고 지시에 적혀있다.
+		//month의 경우 1월일 경우 1018번 문제와 같이 "%d"로 코딩할 경우 0000.0.0으로 나오게 되니깐 꼭 몇 개의 숫자로 표현해라라고 지정을해주자.
 		
 		scan.close();
 		
