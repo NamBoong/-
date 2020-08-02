010c4bd88a37a5aa61ffdc9edd8f9d5df3109407
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-02 15:48:41 +0900

    코드업 1016번 풀이

---
+++ codeup/src/codeup/Main1016.java
@@ -0,0 +1,23 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Main1016 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int value1 = scan.nextInt();
+		
+		System.out.println(value1);
+		System.out.println(value1);
+		System.out.println(value1);
+		// 3번 출력하자고 했으니 3번 적어주면 끝! 문제가 나름 간단했다
+		
+		scan.close();
+		
+
+	}
+
+}
