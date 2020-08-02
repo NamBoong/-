278cdb8503a1a602399c6911850b175a2132fbef
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-02 21:38:50 +0900

    코드업 1022번 풀이

---
+++ codeup/src/codeup/Main1022.java
@@ -0,0 +1,19 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Main1022 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		String value1 = scan.nextLine();
+		
+		System.out.println(value1);
+		
+		scan.close();
+
+	}
+
+}
