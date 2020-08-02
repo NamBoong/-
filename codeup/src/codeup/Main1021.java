98271865ef76b86d19e75bf4b10052055517265a
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-02 21:32:15 +0900

    코드업 1021번 풀이

---
+++ codeup/src/codeup/Main1021.java
@@ -0,0 +1,20 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Main1021 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		String value1 = scan.next();
+		
+		System.out.println(value1);
+		
+		scan.close();
+		
+
+	}
+
+}
