cb5c9d43eda8c9b45a31db301c5a931bbf3628de
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-07 01:30:21 +0900

    코드업 1014번 풀이

---
+++ codeup/src/codeup/Main1014.java
@@ -0,0 +1,21 @@
+package codeup;
+import java.util.Scanner;
+
+public class Main1014 {
+
+		public static void main(String[] args) {
+
+			Scanner scan = new Scanner (System.in);
+			
+			String value1 = scan.next();
+			String value2 = scan.next();
+			
+			scan.close();
+
+			System.out.println(value2);
+			System.out.println(value1);
+					
+			
+		}
+
+	}
