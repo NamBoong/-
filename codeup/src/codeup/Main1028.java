33e0d47d212ca9a4b921e00804d0082d0e8d9afd
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-06 18:36:35 +0900

    코드업 1028번 풀이

---
+++ codeup/src/codeup/Main1028.java
@@ -0,0 +1,21 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Main1028 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		long value1 = scan.nextLong();
+		// 긴 숫자로 입력하기위해 long과 관련된 함수를 찾았다.
+		
+		System.out.println(value1);
+		
+		scan.close();
+		
+		
+	}
+
+}
