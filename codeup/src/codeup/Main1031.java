e697b37c6f53b6459627744dd8efd16fc8fe72d7
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-06 19:34:10 +0900

    코드업 1031번 풀이

---
+++ codeup/src/codeup/Main1031.java
@@ -0,0 +1,19 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Main1031 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int value1 = scan.nextInt();
+		
+		System.out.println(Integer.toOctalString(value1));
+		
+		scan.close();
+
+	}
+
+}
