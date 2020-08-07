271038f13ec3f1257d3b6e2c124707f6bbfa3f19
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-07 15:15:28 +0900

    코드업 1037번 풀이

---
+++ codeup/src/codeup/Main1037.java
@@ -0,0 +1,22 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Main1037 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int value1 = scan.nextInt();
+		// 10진수로 시작하니깐 int 변수로 받자
+		char ask1 = (char)value1;
+		
+		System.out.println(ask1);
+		
+		scan.close();
+		
+		
+	}
+
+}
