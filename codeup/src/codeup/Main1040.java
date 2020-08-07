fe8e202844f2096e8999fd60c34cbbbe2193b6bd
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-07 16:31:40 +0900

    코드업 1040번 풀이

---
+++ codeup/src/codeup/Main1040.java
@@ -0,0 +1,18 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Main1040 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int value1 = scan.nextInt();
+		
+		System.out.println(-value1);
+		
+		scan.close();
+	}
+
+}
