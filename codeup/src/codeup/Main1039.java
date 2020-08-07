c7ee2b4d3dcd979bd948d80bb05bafb720ff8dee
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-07 15:54:51 +0900

    코드업 1039번 풀이

---
+++ codeup/src/codeup/Main1039.java
@@ -0,0 +1,21 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Main1039 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		double value1 = scan.nextDouble();
+		double value2 = scan.nextDouble();
+		
+		System.out.printf("%.0f", value1 + value2);
+		
+		scan.close();
+		// 1038번과 문제가 동일하다고 느끼는건 나만의 착각일까...?ㅜㅜ
+		
+	}
+
+}
