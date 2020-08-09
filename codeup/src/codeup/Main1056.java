d5aa5529b87e96d26018ef3edd02807c397c77bd
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-09 12:04:07 +0900

    코드업 1056번 풀이

--- codeup/src/codeup/Main1056.java
+++ codeup/src/codeup/Main1056.java
@@ -11,14 +11,16 @@ public class Main1056 {
 		int value1 = scan.nextInt();
 		int value2 = scan.nextInt();
 		
-		if(value1==1 & value2==1 && value1==0 & value2==0) {
-			System.out.println(0);
-			// 현재 나눠서 작성해도 1과 0 결과값이 같이 나오는 현상이 발생중이다.
-		}
+		if(value1==1 & value2==1) {
+				System.out.println(0);
+			}
+		else if(value1==0 & value2==0) {
+				System.out.println(0);
+			}
 		else {
 			System.out.println(1);
 		}
+			
 		scan.close();
 	}
-
 }
