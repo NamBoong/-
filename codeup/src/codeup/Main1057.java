4c6f1e17800d3f5fc0e3ff7738bd5b70428b8ffa
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-09 12:10:26 +0900

    코드업 1057번 풀이

---
+++ codeup/src/codeup/Main1057.java
@@ -0,0 +1,26 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Main1057 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int value1 = scan.nextInt();
+		int value2 = scan.nextInt();
+		
+		if(value1==1 & value2==1) {
+			System.out.println(1);
+		}
+		else if(value1==0 & value2==0) {
+			System.out.println(1);
+		}
+		else {
+			System.out.println(0);
+		}
+		scan.close();
+	}
+
+}
