ff0ae244dfa411e313e2d81a4a8e472919b3c798
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-13 01:30:59 +0900

    코드업 1080번 풀이

---
+++ codeup/src/codeup/Main1080.java
@@ -0,0 +1,25 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Main1080 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int value1 = scan.nextInt();
+		int i = 0;
+		
+		while(true) {
+			int sum = i++;
+			if(sum>=value1) {
+				System.out.println(sum);
+				break;
+			}
+
+		}
+
+		scan.close();
+	}
+}
