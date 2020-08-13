b010e4337f6011a9aaaab93a8f39a091fe563cf1
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-14 01:14:29 +0900

    코드업 1081번 풀이

---
+++ codeup/src/codeup/Main1081.java
@@ -0,0 +1,24 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Main1081 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int n=scan.nextInt();
+		int m=scan.nextInt();
+		
+		for(int a=1; a<=n; a++) {
+		for(int b=1; b<=m; b++) {
+
+			System.out.println(a + " " + b);
+
+			}
+			}
+		scan.close();
+	}
+}
+
