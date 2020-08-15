9e1da6c740245c495857d6e63db2ac7e95460424
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-15 23:25:43 +0900

    코드업 1082문제 풀이중

--- codeup/src/codeup/Main1082.java
+++ codeup/src/codeup/Main1082.java
@@ -9,23 +9,12 @@ public class Main1082 {
 		Scanner scan = new Scanner(System.in);
 		
 		String value1 = scan.next();
+		int value2 = Integer.parseInt(value1, 16);
 		
-		for(int v=1; v<=16; v++) {
-			//a는 둘의 곱셈 결과
-
-			int a = v;
-			int b = Integer.valueOf(a);
-			String c = Integer.toHexString(b);
-			System.out.println(value1 + "*" + stringToHex(v) + "=" + c );
+		for(int i=1; i<=16; i++) {
+			System.out.format("%X*%X=%X%n", value1, i, value2*i);
 		}
-		
 		scan.close();
 	}
 
-	private static int stringToHex(int v) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
 }
-// 현재 16진수로 곱셈을 못받고 있다. 진수 변경만 되면 될 것 같다.
