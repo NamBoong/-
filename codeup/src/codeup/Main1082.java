06e55ae2228f4dba9351c9b254dc0c73f7d7607d
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-16 00:13:14 +0900

    코드업 1082번 풀이

--- codeup/src/codeup/Main1082.java
+++ codeup/src/codeup/Main1082.java
@@ -7,12 +7,12 @@ public class Main1082 {
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
-		
 		String value1 = scan.next();
-		int value2 = Integer.parseInt(value1, 16);
 		
-		for(int i=1; i<=16; i++) {
-			System.out.format("%X*%X=%X%n", value1, i, value2*i);
+				int value2 = Integer.parseInt(value1, 16);
+		
+		for(int i=1; i<16; i++) {
+			System.out.format("%X*%X=%X%n", value2, i, value2*i);
 		}
 		scan.close();
 	}
