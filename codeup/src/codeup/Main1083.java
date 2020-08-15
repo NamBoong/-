231760f7ecb342b5532a272847ab91d0b808d043
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-16 01:14:27 +0900

    코드업 1083번 풀이

--- codeup/src/codeup/Main1083.java
+++ codeup/src/codeup/Main1083.java
@@ -13,6 +13,7 @@ public class Main1083 {
 		for(int i=1; i<=value1; i++) {
 			if(i%3==0) {
 				System.out.printf("X"+" ");
+				// 띄어쓰기를 하고싶을땐 공백을 추가해서 줄바꿈이 없게 만들어주자.
 			}
 			else{
 				System.out.printf(i+" ");
