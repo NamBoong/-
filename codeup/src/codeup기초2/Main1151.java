fab76f2a5be2db7e41a5abcfe48be2e6063028ea
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-18 19:10:33 +0900

    코드업 1151번 풀이

---
+++ codeup/src/codeup기초2/Main1151.java
@@ -0,0 +1,19 @@
+package codeup±‚√ 2;
+
+import java.util.Scanner;
+
+public class Main1151 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		
+		if(a<10) {
+			System.out.println("small");
+		}
+		scan.close();
+	}
+
+}
