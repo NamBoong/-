351de1aa1212cb9b4ef25c1d9c443fc8e5ffe50c
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-18 16:55:22 +0900

    코드업 1135번 풀이

---
+++ codeup/src/codeup기초2/Main1135.java
@@ -0,0 +1,23 @@
+package codeup±âÃÊ2;
+
+import java.util.Scanner;
+
+public class Main1135 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		if(a>=b) {
+			System.out.println(1);
+		}
+		else {
+			System.out.println(0);
+		}
+		scan.close();
+	}
+
+}
