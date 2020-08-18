213bc60bbde61e273b8857335043e07f09133dc9
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-18 14:29:19 +0900

    코드업 1115번 풀이

---
+++ codeup/src/codeup기초2/Main1115.java
@@ -0,0 +1,18 @@
+package codeup±‚√ 2;
+
+import java.util.Scanner;
+
+public class Main1115 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		long a = scan.nextLong();
+		long b = scan.nextLong();
+		
+		System.out.println(a+b);
+		scan.close();
+	}
+
+}
