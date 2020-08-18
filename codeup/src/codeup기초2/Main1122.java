a690ef07a5d7c7f75767cff9d53c7949104eb6cd
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-18 16:00:02 +0900

    코드업 1122번 풀이

---
+++ codeup/src/codeup기초2/Main1122.java
@@ -0,0 +1,17 @@
+package codeup±‚√ 2;
+
+import java.util.Scanner;
+
+public class Main1122 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		
+		System.out.println(a/60+" "+a%60);
+		scan.close();
+	}
+
+}
