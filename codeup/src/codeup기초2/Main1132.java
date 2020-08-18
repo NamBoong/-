c6f2e3aca03bf3cd8f1b25b5c01f2b677fec712c
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-18 16:37:56 +0900

    코드업 1132번 풀이

---
+++ codeup/src/codeup기초2/Main1132.java
@@ -0,0 +1,17 @@
+package codeup±‚√ 2;
+
+import java.util.Scanner;
+
+public class Main1132 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		String a = scan.nextLine();
+		
+		System.out.println(a);
+		scan.close();
+	}
+
+}
