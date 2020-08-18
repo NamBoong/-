eec3f3edf84dd1d791b09d0d53950c27fd34f6af
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-18 14:24:23 +0900

    코드업 1113번 풀이

---
+++ codeup/src/codeup기초2/Main1113.java
@@ -0,0 +1,19 @@
+package codeup±‚√ 2;
+
+import java.util.Scanner;
+
+public class Main1113 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		System.out.println(b+" "+a);
+		
+		scan.close();
+	}
+
+}
