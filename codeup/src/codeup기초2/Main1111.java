839ef5aa591b5eced17329abb819e7dd4b5275e0
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-18 14:13:34 +0900

    코드업 1111번 풀이

---
+++ codeup/src/codeup기초2/Main1111.java
@@ -0,0 +1,17 @@
+package codeup±‚√ 2;
+
+import java.util.Scanner;
+
+public class Main1111 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		System.out.println(a+"%");
+		
+		scan.close();
+	}
+
+}
