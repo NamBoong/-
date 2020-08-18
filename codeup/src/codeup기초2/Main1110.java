d348199bb618f2a05b3cb57ec1f2a1b2de4b9f87
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-18 13:59:29 +0900

    코드업 1110번 풀이

---
+++ codeup/src/codeup기초2/Main1110.java
@@ -0,0 +1,14 @@
+package codeup±‚√ 2;
+
+import java.util.Scanner;
+
+public class Main1110 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		System.out.println(a);
+	}
+
+}
