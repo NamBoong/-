3608398e7b4de2c2b2c7495b6565362238edf123
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-18 14:45:40 +0900

    코드업 1117번 풀이

---
+++ codeup/src/codeup기초2/Main1117.java
@@ -0,0 +1,18 @@
+package codeup±‚√ 2;
+
+import java.util.Scanner;
+
+public class Main1117 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		double a = scan.nextDouble();
+		double b = scan.nextDouble();
+		
+		System.out.printf("%.2f", a*b);
+		scan.close();
+	}
+
+}
