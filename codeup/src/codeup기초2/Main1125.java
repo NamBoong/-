6092ab61a5d0ec8f9f2284545fc3d82ebc5a2cc2
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-18 16:29:57 +0900

    코드업 1125번 풀이

---
+++ codeup/src/codeup기초2/Main1125.java
@@ -0,0 +1,17 @@
+package codeup±‚√ 2;
+
+import java.util.Scanner;
+
+public class Main1125 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		
+		System.out.println(String.format("%o", a) +" "+ String.format("%X", a));
+		scan.close();
+	}
+
+}
