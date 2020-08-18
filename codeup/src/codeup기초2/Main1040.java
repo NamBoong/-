482560e51bff25b3daf2e46e78227e177fae8a72
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-18 17:32:53 +0900

    코드업 1140번 풀이

---
+++ codeup/src/codeup기초2/Main1040.java
@@ -0,0 +1,23 @@
+package codeup±‚√ 2;
+
+import java.util.Scanner;
+
+public class Main1040 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		if(a==1 || b==1) {
+			System.out.println(1);
+		}
+		else {
+			System.out.println(0);
+		}
+		scan.close();
+	}
+
+}
