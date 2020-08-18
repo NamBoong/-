e6fad22ab8f6e828f001abd50305b763eed5426a
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-18 14:26:18 +0900

    코드업 1114번 풀이

---
+++ codeup/src/codeup기초2/Main1114.java
@@ -0,0 +1,19 @@
+package codeup±‚√ 2;
+
+import java.util.Scanner;
+
+public class Main1114 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		System.out.println(a+b);
+		
+		scan.close();
+	}
+
+}
