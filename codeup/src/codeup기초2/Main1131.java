ea340a69b9451e2029a3577f9d5b1d2da3abdad8
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-18 16:34:03 +0900

    코드업 1131번 풀이

---
+++ codeup/src/codeup기초2/Main1131.java
@@ -0,0 +1,17 @@
+package codeup±‚√ 2;
+
+import java.util.Scanner;
+
+public class Main1131 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		String a = scan.next();
+		
+		System.out.println(a);
+		scan.close();
+	}
+
+}
