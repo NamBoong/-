e0d8dc19977fb5e8afc08e9d82bea76017df8829
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-18 16:12:37 +0900

    코드업 1123번 풀이

---
+++ codeup/src/codeup기초2/Main1123.java
@@ -0,0 +1,18 @@
+package codeup±‚√ 2;
+
+import java.util.Scanner;
+
+public class Main1123 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		double a = scan.nextDouble();
+		
+		System.out.printf("%.3f", 1.8*a+32);
+		
+		scan.close();
+	}
+
+}
