3f64100f110ae7501b44c01a70ceeb3aefadcf18
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-20 00:17:56 +0900

    코드업 1171번 풀이

---
+++ codeup/src/codeup기초2/Main1171.java
@@ -0,0 +1,42 @@
+package codeup±âÃÊ2;
+
+import java.util.Scanner;
+
+public class Main1171 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		
+		if(c>=100) { 
+			if(b>=10) {
+			System.out.println(a+""+b+""+c);
+			}
+			if(b<10) {
+				System.out.println(a+"0"+b+""+c);
+			}
+		}
+		if(10<=c && c<100) {
+			if(b>=10) {
+				System.out.println(a+""+b+"0"+c);
+				}
+				if(b<10) {
+					System.out.println(a+"0"+b+"0"+c);
+				}
+		}
+		if(10>c) {
+			if(b>=10) {
+				System.out.println(a+""+b+"00"+c);
+				}
+				if(b<10) {
+					System.out.println(a+"0"+b+"00"+c);
+				}
+		}
+		scan.close();
+	}
+
+}
