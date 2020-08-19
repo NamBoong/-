66af5c0c7c76e2da930ad95a0a875920dabe867a
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-19 15:39:59 +0900

    코드업 1166번 풀이

---
+++ codeup/src/codeup기초2/Main1166.java
@@ -0,0 +1,22 @@
+package codeup±‚√ 2;
+
+import java.util.Scanner;
+
+public class Main1166 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+
+		if (a%400==0 || (a%4==0 && a%100!=0)) {
+			System.out.println("yes");
+		}
+		else {
+			System.out.println("no");
+		}
+		scan.close();
+	}
+
+}
