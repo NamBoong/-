f25f3853c8c61612cdc0fbea16d2918a00e0c331
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-19 14:19:26 +0900

    코드업 1164번 풀이

---
+++ codeup/src/codeup기초2/Main1164.java
@@ -0,0 +1,24 @@
+package codeup±âÃÊ2;
+
+import java.util.Scanner;
+
+public class Main1164 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		
+		if(170>=a || 170>=b || 170>=c) {
+			System.out.println("CRASH");
+		}
+		else {
+			System.out.println("PASS");
+		}
+		scan.close();
+	}
+
+}
