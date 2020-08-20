7021911cebe671295e1cd30fa8d6bc254562c757
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-21 00:38:30 +0900

    코드업 1206번 풀이

---
+++ codeup/src/codeup기초2/Main1206.java
@@ -0,0 +1,31 @@
+package codeup±‚√ 2;
+
+import java.util.Scanner;
+
+public class Main1206 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		while(true) {
+			if(b%a==0) {
+				System.out.println(a+"*"+(b/a)+"="+b);
+				break;
+			}
+			if(a%b==0) {
+				System.out.println(b+"*"+(a/b)+"="+a);
+				break;
+			}
+			if(a%b!=0 && b%a!=0) {
+				System.out.println("none");
+				break;
+			}
+		}
+		scan.close();
+		}
+
+}
