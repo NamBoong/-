d80520b51a6caa66ddf64182a1d900c65dd06781
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-21 00:19:50 +0900

    코드업 1205번 풀이

---
+++ codeup/src/codeup기초2/Main1205.java
@@ -0,0 +1,34 @@
+package codeup±‚√ 2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main1205 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int x = scan.nextInt();
+		int y = scan.nextInt();
+		
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(x+y);
+		list.add(x-y);
+		list.add(y-x);
+		list.add(x*y);
+		list.add(x/y);
+		list.add(y/x);
+		list.add((int) Math.pow(x, y));
+		list.add((int) Math.pow(y, x));
+		
+		Integer i = Collections.max(list);
+		
+		double j = i;
+		System.out.printf("%.6f", j);
+		
+		scan.close();
+	}
+
+}
