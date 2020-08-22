fbc2913ea090b6b24e099f799d9bba67ef430f9d
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-22 22:34:53 +0900

    코드업 1230번 풀이

---
+++ codeup/src/codeup기초2/Main1230.java
@@ -0,0 +1,36 @@
+package codeup±‚√ 2;
+
+import java.util.Scanner;
+
+public class Main1230 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		
+		while(true) {
+			if(170<a && 170<b && 170<c) {
+				System.out.println("PASS");
+				break;
+			}
+			if(170>=a) {
+				System.out.println("CRASH "+a);
+				break;
+			}
+			if(170>=b) {
+				System.out.println("CRASH "+b);
+				break;
+			}
+			if(170>=c) {
+				System.out.println("CRASH "+c);
+				break;
+			}
+		}
+		scan.close();
+	}
+
+}
