30575cd0a8b36458e1e454181880cf257d026ef8
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-23 15:47:17 +0900

    코드업 1252번 풀이

---
+++ codeup/src/codeup기초4/Main1252.java
@@ -0,0 +1,19 @@
+package codeup±‚√ 4;
+
+import java.util.Scanner;
+
+public class Main1252 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		for(int a=1; a<=n; a++) {
+			System.out.printf(a+" ");
+		}
+	scan.close();
+	}
+
+}
