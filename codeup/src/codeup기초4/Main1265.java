2b26bcf7c0a5f901d4a3c0dd4563ca3d6bb0750d
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-23 22:27:59 +0900

    코드업 1265번 풀이

---
+++ codeup/src/codeup기초4/Main1265.java
@@ -0,0 +1,19 @@
+package codeup±‚√ 4;
+
+import java.util.Scanner;
+
+public class Main1265 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		
+		for(int i=1; i<=9; i++) {
+			System.out.println(a+"*"+i+"="+(a*i));
+		}
+		scan.close();
+	}
+
+}
