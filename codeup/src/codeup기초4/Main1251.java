fb0d97db3b59296d00fab3e36569e1d50b50098d
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-23 15:44:58 +0900

    코드업 1251번 풀이

---
+++ codeup/src/codeup기초4/Main1251.java
@@ -0,0 +1,17 @@
+package codeup±‚√ 4;
+
+import java.util.Scanner;
+
+public class Main1251 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		for(int a=1; a<=100; a++) {
+			System.out.printf(a+" ");
+		}
+	scan.close();
+	}
+
+}
