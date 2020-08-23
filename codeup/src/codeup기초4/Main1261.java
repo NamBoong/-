c804fbad7f01bd3eb1faa6df2a4af3c7750771b2
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-23 22:22:18 +0900

    코드업 1261번 풀이

---
+++ codeup/src/codeup기초4/Main1261.java
@@ -0,0 +1,71 @@
+package codeup±‚√ 4;
+
+import java.util.Scanner;
+
+public class Main1261 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int a1 = scan.nextInt();
+		int a2 = scan.nextInt();
+		int a3 = scan.nextInt();
+		int a4 = scan.nextInt();
+		int a5 = scan.nextInt();
+		int a6 = scan.nextInt();
+		int a7 = scan.nextInt();
+		int a8 = scan.nextInt();
+		int a9 = scan.nextInt();
+		int a10 = scan.nextInt();
+
+		while(true) {
+			if(a1%5==0) {
+				System.out.println(a1);
+				break;
+			}
+			if(a2%5==0) {
+				System.out.println(a2);
+				break;
+			}
+			if(a3%5==0) {
+				System.out.println(a3);
+				break;
+			}
+			if(a4%5==0) {
+				System.out.println(a4);
+				break;
+			}
+			if(a5%5==0) {
+				System.out.println(a5);
+				break;
+			}
+			if(a6%5==0) {
+				System.out.println(a6);
+				break;
+			}
+			if(a7%5==0) {
+				System.out.println(a7);
+				break;
+			}
+			if(a8%5==0) {
+				System.out.println(a8);
+				break;
+			}
+			if(a9%5==0) {
+				System.out.println(a9);
+				break;
+			}
+			if(a10%5==0) {
+				System.out.println(a10);
+				break;
+			}
+			else {
+				System.out.println("0");
+				break;
+			}
+		}
+		scan.close();
+	}
+}
+		
