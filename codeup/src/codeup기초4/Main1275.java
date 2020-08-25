ad0fdfda28f41f066a1e183e6d6ed8d3107d9247
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-25 12:16:58 +0900

    코드업 1275번 풀이

---
+++ codeup/src/codeup기초4/Main1275.java
@@ -0,0 +1,18 @@
+package codeup±‚√ 4;
+
+import java.util.Scanner;
+
+public class Main1275 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		System.out.printf("%.0f", Math.pow(a, b));
+		scan.close();
+	}
+
+}
