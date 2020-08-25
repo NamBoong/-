62c4abe4b54fe19146f13a652a648f52249973d7
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-25 16:36:33 +0900

    코드업 1280번 풀이

---
+++ codeup/src/codeup기초4/Main1280.java
@@ -0,0 +1,29 @@
+package codeup±‚√ 4;
+
+import java.util.Scanner;
+
+public class Main1280 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int answer1 = 0;
+		int answer2 = 0;
+		
+		for(int i=a; i<=b; i++) {
+			if(i%2==0) {
+				answer1 = answer1-i;
+				System.out.printf("-"+i);
+			}
+			if(i%2!=0) {
+				answer2 = answer2+i;
+				System.out.printf("+"+i);
+			}
+		}
+		System.out.printf("=" + (answer1+answer2));
+	}
+
+}
