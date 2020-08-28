62ea87f7c12dac0eb8d4e1839d23c9e117a6ff3b
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-28 14:54:09 +0900

    코드업 1382번 풀이

---
+++ codeup/src/codeup기초4_2/Main1382.java
@@ -0,0 +1,15 @@
+package codeup±‚√ 4_2;
+
+public class Main1382 {
+
+	public static void main(String[] args) {
+
+		for(int b=1; b<=9; b++) {
+		for(int a=2; a<=5; a++) {
+			System.out.printf("%d x %d = %2d\t", a, b, (a*b));			
+		}
+			System.out.printf("\n");
+		}
+	}
+
+}
