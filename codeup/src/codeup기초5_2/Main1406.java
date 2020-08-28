7bb2ac58a3ddab9d1334217d709bd45d58a126bb
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-28 23:34:35 +0900

    코드업 1406번 풀이

---
+++ codeup/src/codeup기초5_2/Main1406.java
@@ -0,0 +1,34 @@
+package codeup±‚√ 5_2;
+
+import java.util.Scanner;
+
+public class Main1406 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		String a = scan.nextLine();
+		int[] arr = new int[4];
+		
+
+		for(int x=0; x<4; x++) {
+			arr[x] = a.charAt(x);
+		
+		if(a.length()==4) {
+		if(arr[0]==108) {
+		if(arr[1]==111) {
+		if(arr[2]==118) {
+		if(arr[3]==101) {
+			System.out.println("I love you.");
+		}
+		}
+		}
+		}			
+		}
+		}
+		scan.close();
+
+	}
+
+}
