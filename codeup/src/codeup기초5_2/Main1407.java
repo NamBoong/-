37e0fb6fee55362e4a8893592ea700f89b204e34
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-28 23:50:08 +0900

    코드업 1407번 풀이

---
+++ codeup/src/codeup기초5_2/Main1407.java
@@ -0,0 +1,16 @@
+package codeup±‚√ 5_2;
+
+import java.util.Scanner;
+
+public class Main1407 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		String a = scan.nextLine();
+		System.out.println(a.replaceAll(" ", ""));
+		scan.close();
+	}
+
+}
