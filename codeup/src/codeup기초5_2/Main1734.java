4480513cd58f70d048b8334200f5164469714e30
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-29 15:04:11 +0900

    코드업 1734번 풀이

---
+++ codeup/src/codeup기초5_2/Main1734.java
@@ -0,0 +1,17 @@
+package codeup±‚√ 5_2;
+
+import java.util.Scanner;
+
+public class Main1734 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		String a = scan.nextLine();
+		
+		System.out.printf("welcome! "+a);
+		scan.close();
+	}
+
+}
