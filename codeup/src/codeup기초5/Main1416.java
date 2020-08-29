27e40b2e9fd904638e8acc1586f2d62c409db963
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-30 00:41:57 +0900

    코드업 1416번 풀이

---
+++ codeup/src/codeup기초5/Main1416.java
@@ -0,0 +1,19 @@
+package codeup±‚√ 5;
+
+import java.util.Scanner;
+
+public class Main1416 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		String n2 = Integer.toBinaryString(n);
+		
+		System.out.println(n2);
+		scan.close();
+	}
+
+}
