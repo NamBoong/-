62c7a198a57196ef1c015c1e5207ebc5c6d3c5af
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-08-29 15:39:29 +0900

    코드업 1990번 풀이

---
+++ codeup/src/codeup기초5_2/Main1990.java
@@ -0,0 +1,22 @@
+package codeup±‚√ 5_2;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main1990 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		BigInteger a = scan.nextBigInteger();
+		
+		if(a.remainder(BigInteger.valueOf(3)) != BigInteger.valueOf(0)) {
+			System.out.println(0);
+		}
+		if(a.remainder(BigInteger.valueOf(3)) == BigInteger.valueOf(0)) {
+			System.out.println(1);
+		}
+	}
+
+}
