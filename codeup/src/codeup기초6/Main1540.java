3ea8af1e93a3fadf51d3528c05d897f381dc82e9
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-01 21:09:08 +0900

    코드업 1540번 풀이

---
+++ codeup/src/codeup기초6/Main1540.java
@@ -0,0 +1,22 @@
+package codeup±‚√ 6;
+
+import java.util.Scanner;
+
+public class Main1540 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		if(n==0) {
+			System.out.println("zero");
+		}
+		else {
+			System.out.println("non zero");
+		}
+		scan.close();
+	}
+
+}
