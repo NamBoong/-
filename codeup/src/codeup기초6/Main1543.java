487a1457329150202f1439d99e0196fb3631685e
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-01 21:23:29 +0900

    코드업 1543번 풀이

---
+++ codeup/src/codeup기초6/Main1543.java
@@ -0,0 +1,19 @@
+package codeup±‚√ 6;
+
+import java.util.Scanner;
+
+public class Main1543 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		for(int a=1; a<=n; a++) {
+			System.out.println("love");
+		}
+		scan.close();
+	}
+
+}
