477f191e45ad4b0feac902f63d0cfcd6bb6f01e0
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-01 21:31:19 +0900

    코드업 1545번 풀이

---
+++ codeup/src/codeup기초6/Main1545.java
@@ -0,0 +1,23 @@
+package codeup±‚√ 6;
+
+import java.util.Scanner;
+
+public class Main1545 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		if(n==0) {
+			System.out.println("zero");
+		}
+		
+		else {
+			System.out.println("non zero");
+		}
+		scan.close();
+	}
+
+}
