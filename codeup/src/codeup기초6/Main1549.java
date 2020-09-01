06ff1407d6d9164e655b4e30e6564a6f24cdc492
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-01 22:28:53 +0900

    코드업 1549번 풀이

---
+++ codeup/src/codeup기초6/Main1549.java
@@ -0,0 +1,17 @@
+package codeup±‚√ 6;
+
+import java.util.Scanner;
+
+public class Main1549 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		long n = scan.nextLong();
+		
+		System.out.println(Math.abs(n));
+		scan.close();
+	}
+
+}
