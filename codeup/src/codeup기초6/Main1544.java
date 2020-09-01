f9f81802d635b2e2858fc42f38271b4345567062
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-01 21:25:55 +0900

    코드업 1544번 풀이

---
+++ codeup/src/codeup기초6/Main1544.java
@@ -0,0 +1,19 @@
+package codeup±‚√ 6;
+
+import java.util.Scanner;
+
+public class Main1544 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			System.out.printf("*");
+		}
+		scan.close();
+	}
+
+}
