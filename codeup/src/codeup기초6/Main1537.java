36b85e3de615a5bb9f96434c8f47fbc73ff114d9
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-01 20:54:01 +0900

    코드업 1537번 풀이

---
+++ codeup/src/codeup기초6/Main1537.java
@@ -0,0 +1,22 @@
+package codeup±‚√ 6;
+
+import java.util.Scanner;
+
+public class Main1537 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		if(n==1) {
+			System.out.println("hello");
+		}
+		if(n==2) {
+			System.out.println("world");
+		}
+		scan.close();
+	}
+
+}
