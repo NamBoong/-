9ea62ec9989ca36578d7330c39a6b7f9e2abeb84
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-01 21:02:47 +0900

    코드업 1539번 풀이

---
+++ codeup/src/codeup기초6/Main1539.java
@@ -0,0 +1,22 @@
+package codeup±‚√ 6;
+
+import java.util.Scanner;
+
+public class Main1539 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		if(n==0) {
+			System.out.println("false");
+		}
+		else {
+			System.out.println("true");
+		}
+		scan.close();
+	}
+
+}
