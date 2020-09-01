a2bc71f36c038a889924130ca03b24a9b72e5f38
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-01 21:51:37 +0900

    코드업 1547번 풀이

---
+++ codeup/src/codeup기초6/Main1547.java
@@ -0,0 +1,30 @@
+package codeup±‚√ 6;
+
+import java.util.Scanner;
+
+public class Main1547 {
+
+	public static void main(String[] args) {
+	
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		int count = 0;
+		
+		for(int i=2; i<=n; i++) {
+			if(n%i==0) {
+				count ++;
+			}
+		}
+		
+		if(count==1) {
+			System.out.println("prime");
+		}
+		else {
+			System.out.println("composite");
+		}
+		scan.close();
+		
+	}
+
+}
