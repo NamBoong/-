2d4dec3ad271354cb25511facdb82de7770b4c62
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-01 23:40:56 +0900

    코드업 1554번 풀이

---
+++ codeup/src/codeup기초6/Main1554.java
@@ -0,0 +1,17 @@
+package codeup±‚√ 6;
+
+import java.util.Scanner;
+
+public class Main1554 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		double n = scan.nextDouble();
+		
+		System.out.format("%.0f", Math.floor(n));
+		scan.close();
+	}
+
+}
