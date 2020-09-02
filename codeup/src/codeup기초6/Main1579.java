a357501bb3c044bc01a94aca9b262817d312e8b8
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-02 21:44:43 +0900

    코드업 1579번 풀이

---
+++ codeup/src/codeup기초6/Main1579.java
@@ -0,0 +1,18 @@
+package codeup±‚√ 6;
+
+import java.util.Scanner;
+
+public class Main1579 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		System.out.println(Math.min(a, b));
+		scan.close();
+	}
+
+}
