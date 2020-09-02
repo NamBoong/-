1f3597f731eff2047f9e06261bd910b790a4ffa6
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-02 21:39:06 +0900

    코드업 1577번 풀이

---
+++ codeup/src/codeup기초6/Main1577.java
@@ -0,0 +1,17 @@
+package codeup±‚√ 6;
+
+import java.util.Scanner;
+
+public class Main1577 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		System.out.println(Math.abs(n));
+		scan.close();
+	}
+
+}
