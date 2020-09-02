f29026e7e2d5bab9f69f0ed58df1e93a375f2031
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-02 21:42:55 +0900

    코드업 1578번 풀이

---
+++ codeup/src/codeup기초6/Main1578.java
@@ -0,0 +1,18 @@
+package codeup±‚√ 6;
+
+import java.util.Scanner;
+
+public class Main1578 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		System.out.println(Math.max(a, b));
+		scan.close();
+	}
+
+}
