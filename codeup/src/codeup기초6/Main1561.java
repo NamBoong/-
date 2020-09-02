43dd8357bc499ea0d7fa548215def9f75567cc4f
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-02 17:06:10 +0900

    코드업 1561번 풀이

---
+++ codeup/src/codeup기초6/Main1561.java
@@ -0,0 +1,28 @@
+package codeup±âÃÊ6;
+
+import java.util.Scanner;
+
+public class Main1561 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		if(a>b) {
+			System.out.println(a);
+		}
+		
+		if(a<b) {
+			System.out.println(b);
+		}
+		
+		if(a==b) {
+			System.out.println(0);
+		}
+		scan.close();
+	}
+
+}
