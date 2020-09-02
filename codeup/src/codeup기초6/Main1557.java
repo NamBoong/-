0233af4e59758d9173ce444459b2800d90cc6a51
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-02 13:33:06 +0900

    코드업 1557번 풀이

---
+++ codeup/src/codeup기초6/Main1557.java
@@ -0,0 +1,23 @@
+package codeup±‚√ 6;
+
+import java.util.Scanner;
+
+public class Main1557 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		int count = 0;
+		
+		for(int i=1; i<=n; i++) {
+			if(n%i==0) {
+				count++;
+			}
+		}
+		System.out.println(count);
+		scan.close();
+	}
+
+}
