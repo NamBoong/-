d5426ee01a4580e922805de4d87a1fdfb5133e0d
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-02 19:38:27 +0900

    코드업 1570번 풀이

---
+++ codeup/src/codeup기초6/Main1570.java
@@ -0,0 +1,38 @@
+package codeup±‚√ 6;
+
+import java.util.Scanner;
+
+public class Main1570 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		int[] arr = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			arr[i] = scan.nextInt();
+		}
+		
+		int k = scan.nextInt();
+		int count = 0;
+		
+		for(int i=0; i<n; i++) {
+			if(arr[i]>=k) {
+				System.out.println(i+1);
+				break;
+			}
+			if(arr[i]<k) {
+				count++;
+			}
+		}
+		
+		if(count==n) {
+			System.out.println(n+1);
+		}
+		scan.close();
+	}
+
+}
