1310048272fcc6488f90f5a3ae3d38398e1045e1
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-02 21:47:16 +0900

    코드업 1580번 풀이

---
+++ codeup/src/codeup기초6/Main1580.java
@@ -0,0 +1,17 @@
+package codeup±‚√ 6;
+
+import java.util.Scanner;
+
+public class Main1580 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int r = scan.nextInt();
+		
+		System.out.format("%.2f", r*r*3.14);
+		scan.close();
+	}
+
+}
