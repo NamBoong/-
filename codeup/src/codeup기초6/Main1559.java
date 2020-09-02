c355993f37f699eea3e82c4c0a0b55bc2b9d84b7
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-02 16:57:43 +0900

    코드업 1559번 풀이

---
+++ codeup/src/codeup기초6/Main1559.java
@@ -0,0 +1,19 @@
+package codeup±‚√ 6;
+
+import java.util.Scanner;
+
+public class Main1559 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		long a = scan.nextLong();
+		long b = scan.nextLong();
+		
+		System.out.println(a+b);			
+		scan.close();
+		
+	}
+
+}
