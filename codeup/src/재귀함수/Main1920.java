d3bc1c268ef98025ae746bbdbbb9ea2e0c04361d
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-05 23:31:24 +0900

    코드업 1920번 재귀함수 안 쓴 풀이

---
+++ codeup/src/재귀함수/Main1920.java
@@ -0,0 +1,18 @@
+package Àç±ÍÇÔ¼ö;
+
+import java.util.Scanner;
+
+public class Main1920 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		System.out.println(Integer.toBinaryString(n));
+		
+		scan.close();
+	}
+
+}
