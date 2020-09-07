38f2324b22e69b8c8823a49d98819b69aa903cd5
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-07 14:04:37 +0900

    코드업 4021번 풀이

---
+++ codeup/src/정을1번수준문제/Main4021.java
@@ -0,0 +1,34 @@
+package 정을1번수준문제;
+
+import java.util.Scanner;
+
+public class Main4021 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int[] arr = new int[7];
+		int answer = 0;
+		int count = 0;
+		
+		for(int i=0; i<7; i++) {
+			arr[i] = scan.nextInt();
+			
+			if(arr[i]%2==1) {
+				answer = answer + arr[i];
+			}
+			
+			if(arr[i]%2==0) {
+				count++;
+				if(count==7) {
+					answer = -1;
+				}
+			}
+		}
+		
+		System.out.println(answer);
+		scan.close();
+	}
+
+}
