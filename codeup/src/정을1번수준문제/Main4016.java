871f13cc0003eb5ef5e111116eb1db7c0634feb8
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-07 13:54:18 +0900

    코드업 4016번 풀이

---
+++ codeup/src/정을1번수준문제/Main4016.java
@@ -0,0 +1,26 @@
+package 정을1번수준문제;
+
+import java.util.Scanner;
+
+public class Main4016 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		int answer = 0;
+		
+		for(int i=1; i<=Math.max(Math.max(a, b),c); i++) {
+			if(a%i==0 && b%i==0 && c%i==0) {
+				answer = i;
+			}
+		}
+		
+		System.out.println(answer);
+		scan.close();
+	}
+
+}
