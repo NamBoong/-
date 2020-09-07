e1ed61061fd425ce8f94b530bcc9667ca660a19c
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-07 13:47:47 +0900

    코드업 4013번 풀이

---
+++ codeup/src/정을1번수준문제/Main4013.java
@@ -0,0 +1,20 @@
+package 정을1번수준문제;
+
+import java.util.Scanner;
+
+public class Main4013 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		System.out.printf(2 + " "+ Integer.toBinaryString(n) + "\n");
+		System.out.printf(8 + " " + Integer.toOctalString(n) + "\n");
+		System.out.printf("16 %X", n);
+		
+		scan.close();
+	}
+
+}
