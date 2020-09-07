a5f7dab3eb435777998b38be868f986560a9bc8b
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-07 15:14:28 +0900

    코드업 4031번 풀이

---
+++ codeup/src/정을1번수준문제/Main4031.java
@@ -0,0 +1,43 @@
+package 정을1번수준문제;
+
+import java.util.Scanner;
+
+public class Main4031 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int[] arr = new int[7];
+		
+		for(int i=0; i<7; i++) {
+			arr[i] = scan.nextInt();
+		}
+		
+		for(int i=0; i<7; i++) {
+			for(int j=i+1; j<7; j++) {
+				if(arr[i]>arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		
+		int even = 0;
+		int odd = 0;
+	
+		for(int i=0; i<7; i++) {
+			if(arr[i]%2==0) {
+				even = arr[i];
+			}
+			if(arr[i]%2==1) {
+				odd = arr[i];
+			}
+		}
+
+		System.out.println(even+odd);
+		scan.close();
+	}
+
+}
