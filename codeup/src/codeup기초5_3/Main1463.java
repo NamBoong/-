2168fb8a46fcc6fd54ca5a9afb3d3d825d8d091b
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-11 01:12:01 +0900

    코드업 1463번 풀이

---
+++ codeup/src/codeup기초5_3/Main1463.java
@@ -0,0 +1,33 @@
+package codeup±‚√ 5_3;
+
+import java.util.Scanner;
+
+public class Main1463 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		int[][] arr = new int[n][n];
+		
+		int count = 1;
+
+		for(int j=0; j<n; j++) {
+			for(int i=n-1; i>=0; i--) {
+
+				arr[i][j] = count;
+				count++;
+			}
+		}
+					
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				System.out.printf(arr[i][j] + " ");
+			}
+			System.out.printf("\n");
+		}
+		scan.close();
+	}
+
+}
