866c131817cc9820d824face46b2cb195d5d5136
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-11 16:48:24 +0900

    코드업 1471번 풀이

---
+++ codeup/src/codeup기초5_3/Main1471.java
@@ -0,0 +1,41 @@
+package codeup±‚√ 5_3;
+
+import java.util.Scanner;
+
+public class Main1471 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		int[][] arr = new int[n][n];
+		
+		int count =1;
+
+		for(int j=0; j<n; j++) {
+			for(int i=n-1; i>=0; i--) {
+			if(j%2==0) {
+				arr[i][j] = count;
+				count++;
+			}
+			}
+			
+			for(int i=0; i<n; i++) {
+			if(j%2==1) {
+				arr[i][j] = count;
+				count++;
+			}
+			}
+		}
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				System.out.printf(arr[i][j] + " ");
+			}
+			System.out.printf("\n");
+		}
+		scan.close();
+	}
+
+}
