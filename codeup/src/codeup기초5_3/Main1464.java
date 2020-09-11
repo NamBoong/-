e32963dd3c09ca9870c34ded3578d0d5c53c00a4
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-11 10:37:53 +0900

    코드업 1464번 풀이

---
+++ codeup/src/codeup기초5_3/Main1464.java
@@ -0,0 +1,34 @@
+package codeup±âÃÊ5_3;
+
+import java.util.Scanner;
+
+public class Main1464 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		int m = scan.nextInt();
+		
+		int[][] arr = new int[n][m];
+		
+		int count = 1;
+		
+		for(int i=n-1; i>=0; i--) {
+			for(int j=m-1; j>=0; j--) {
+				arr[i][j] = count;
+				count++;
+			}
+		}
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<m; j++) {
+				System.out.printf(arr[i][j] + " ");
+			}
+			System.out.printf("\n");
+		}
+		scan.close();
+	}
+
+}
