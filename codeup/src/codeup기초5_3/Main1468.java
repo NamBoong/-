0f6d44379ee3d0fa976c7ca7e868b4ef32326c5d
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-11 12:00:47 +0900

    코드업 1468번 풀이

---
+++ codeup/src/codeup기초5_3/Main1468.java
@@ -0,0 +1,44 @@
+package codeup±‚√ 5_3;
+
+import java.util.Scanner;
+
+public class Main1468 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		int[][] arr = new int[n][n];
+		
+		int count = 1;
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+			if(i%2==0) {
+				arr[i][j] = count;
+				count++;
+			}
+			}
+			
+			for(int j=n-1; j>=0; j--) {
+			if(i%2==1) {
+				arr[i][j] = count;
+				count++;
+			}
+			}
+			
+		}
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				System.out.printf(arr[i][j] + " ");
+			}
+			System.out.printf("\n");
+		}
+		scan.close();
+		
+	}
+
+}
