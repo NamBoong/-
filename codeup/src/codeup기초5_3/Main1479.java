6a0fff9ddb1c186617f3e05fe3523c0b029ce8ee
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-13 08:55:52 +0900

    코드업 1479번 풀이

---
+++ codeup/src/codeup기초5_3/Main1479.java
@@ -0,0 +1,37 @@
+package codeup±‚√ 5_3;
+
+import java.util.Scanner;
+
+public class Main1479 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		int m = scan.nextInt();
+		int[][] arr = new int[n][m];
+		int count = 1;
+		
+		for(int i=0; i<n+m-1; i++) {
+			for(int k=m-1; k>=0; k--) {
+			for(int j=0; j<n; j++) {
+					if(j +(m-1-k) ==i) {
+						arr[j][k] = count;
+						count++;
+					}
+				}
+			}
+		}
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<m; j++) {
+				System.out.printf(arr[i][j] + " ");
+			}
+			System.out.printf("\n");
+		}
+		scan.close();
+		
+	}
+
+}
