7835ea4c0d1376fb52af955fbfb0e6dbb4e873f9
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-14 20:35:29 +0900

    코드업 1481번 풀이

---
+++ codeup/src/codeup기초5_3/Main1481.java
@@ -0,0 +1,37 @@
+package codeup±âÃÊ5_3;
+
+import java.util.Scanner;
+
+public class Main1481 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		int m = scan.nextInt();
+		int[][] arr = new int[n][m];
+		int count = 1;
+		
+		for(int i=n+m-1; i>=0; i--) {
+			for(int j=n-1; j>=0; j--) {
+			for(int k=m-1; k>=0; k--) {
+					if(i -k == j) {
+						arr[j][k] = count;
+						count++;
+					}
+				}
+			}
+		}
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<m; j++) {
+				System.out.printf(arr[i][j] + " ");
+			}
+			System.out.printf("\n");
+		}
+		scan.close();
+		
+	}
+
+}
