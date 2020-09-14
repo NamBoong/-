e3f6a1fb6c68c2d616a58302b1c0f5ab8a34f6f1
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-14 17:08:29 +0900

    코드업 2016번 풀이

---
+++ codeup/src/스택/Main2016.java
@@ -0,0 +1,18 @@
+package Ω∫≈√;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main2016 {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		BigInteger b = scan.nextBigInteger();
+		BigInteger a = scan.nextBigInteger();
+		System.out.format("%,d", a);
+		scan.close();
+	}
+
+}
