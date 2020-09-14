66d775347a605dadf6101f2b3343ed3a6f6f552c
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-14 15:54:17 +0900

    코드업 3021번 풀이

---
+++ codeup/src/스택/Main3021.java
@@ -0,0 +1,20 @@
+package Ω∫≈√;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main3021 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		BigInteger a = scan.nextBigInteger();
+		BigInteger b = scan.nextBigInteger();
+
+		
+		System.out.println(a.add(b));
+		scan.close();
+	}
+
+}
