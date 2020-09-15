fdddc9a2e67e207899c97f9342c17b96d244e387
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-15 15:59:36 +0900

    코드업 1484번 풀이

--- codeup/src/codeup기초5_3/Main1484.java
+++ codeup/src/codeup기초5_3/Main1484.java
@@ -10,28 +10,73 @@ public class Main1484 {
 		
 		int n = scan.nextInt();
 		int m = scan.nextInt();
-		int[][] arr = new int[n][m];
-		int count = 1;
-
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<m; j++) {
-				
-				
-					arr[i][j] = count;
-					count++;
-					System.out.printf(arr[i][j] + " ");
-				
-				
-				if(j==m-1) {
-					i++;
-					System.out.printf(arr[i][m-1] + " ");
-					count++;
-				}
-				
-			}
-			System.out.printf("\n");
-		}
-		scan.close();
+		 int[][] arr = new int[n][m];
+	        
+	        int count = 1;
+	        int goal = n*m;
+	        int i = 0;
+	        int j = 0;
+	        int width = m;
+	        int height = n-1;
+	        
+	        
+	        while(count<=goal) {
+	        	
+	        	for(int a=0; a<width; a++) {
+	        		arr[i][j] = count;
+	        		j++;
+	        		count++;	        		
+	        	}
+	        	width--;
+	        	j--;
+	        	i++;
+	        	
+	        	for(int a=0; a<height; a++) {
+	        		arr[i][j] = count;
+	        		i++;
+	        		count++;
+	        		
+	        	}
+	        	height--;
+	        	i--;
+	        	j--;
+	        	if(count>=goal) {
+	        		break;
+	        	}
+	        	
+	        	
+	        	for(int a=0; a<width; a++) {
+	        			arr[i][j] = count;
+	        			j--;
+	        			count++;
+	        		
+	        	}
+	        	width--;
+	        	j++;
+	        	i--;
+	        	
+	        	for(int a=0; a<height; a++) {
+	        			arr[i][j] = count;
+	        			i--;
+	        			count++;
+	        		
+	        	}
+	        	height--;
+	        	i++;
+	        	j++;
+	        	
+	        	
+	        }
+	        
+	        for(int a=0; a<n; a++) {
+	        	for(int b=0; b<m; b++) {
+	        		System.out.printf(arr[a][b] + " ");
+	        	}
+	        	System.out.println();
+	        }
+	        
+	        scan.close();
 	}
-
 }
+
+// 답은 잘 나오는데 틀렸다고 나와서 그냥 넘기자.
