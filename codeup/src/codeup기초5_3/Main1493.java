c26a596e8e84ae375fb2bdfb69725440928c6c50
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-23 22:52:34 +0900

    코드업 1493번 풀이

--- codeup/src/codeup기초5_3/Main1493.java
+++ codeup/src/codeup기초5_3/Main1493.java
@@ -12,6 +12,7 @@ public class Main1493 {
 		int m = scan.nextInt();
 		int[][] arr = new int[n][m];
 		int[][] arr2 = new int[n][m];
+		int[][] arr3 = new int[n][m];
 		
 		for(int i=0; i<n; i++) {
 			for(int j=0; j<m; j++) {
@@ -20,37 +21,31 @@ public class Main1493 {
 		}
 		
 		int sum = 0;
-		int a = 0;
-		int b = 0;
+		int ycount = 0;
 		
-		
-		// 2번째줄부터 안나오고있음
-		while(a<n && b<m) {		
-
-				for(int k=0; k<a+1; k++) {
-					for(int l=0; l<b+1; l++) {
-						sum = sum + arr[k][l];						
-						arr2[a][b] = sum;
-						
-					}
-					b++;
-					sum = 0;
-					
-				}
-				
-				if(b>m) {
-					b=0;
-					sum = 0;
-					a++;
-				}
-
+		while(ycount<n) {
+			for(int j=0; j<m; j++) {
+				sum = sum + arr[ycount][j];
+				arr2[ycount][j] = sum;
+			}
+			ycount++;
+			sum = 0;
 		}
 		
+		int xcount = 0;
 		
+		while(xcount<m) {
+			for(int i=0; i<n; i++) {
+				sum = sum + arr2[i][xcount];
+				arr3[i][xcount] = sum;
+			}
+			xcount++;
+			sum = 0;
+		}	
 		
 		for(int i=0; i<n; i++) {
 			for(int j=0; j<m; j++) {
-				System.out.printf(arr2[i][j] + " ");
+				System.out.printf(arr3[i][j] + " ");
 			}
 			System.out.println();
 		}
