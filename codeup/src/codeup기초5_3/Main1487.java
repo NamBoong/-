c3c981114c1dbf190e8e4f34ecbc6b19828db547
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-25 13:34:41 +0900

    코드업 1487번 풀이

---
+++ codeup/src/codeup기초5_3/Main1487.java
@@ -0,0 +1,98 @@
+package codeup±âÃÊ5_3;
+
+import java.util.Scanner;
+
+public class Main1487 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+			
+			int n = scan.nextInt();
+			int m = scan.nextInt();
+			 int[][] arr = new int[n][m];
+		        
+		        int count = n*m;
+		        int goal = 1;
+		        int i = 0;
+		        int j = m-1;
+		        int width = m-1;
+		        int height = n;
+		        
+		        
+		        while(count>=goal) {
+		   
+		        	
+		        	
+		        	for(int a=0; a<height; a++) {	
+		        		arr[i][j] = count;		        	
+		        		i++;
+		        		count--;	
+		        		
+		        	}
+		        	
+	        		if(count<goal) {
+		        		break;
+		        	}
+		        	
+		        	height--;
+		        	i--;
+		        	j--;
+		        	
+		        	
+		        	
+		        	for(int a=0; a<width; a++) {
+		        		if(j>=0) {
+		        			arr[i][j] = count;
+		        			j--;
+		        		}
+		        			count--;	
+
+		        	}
+        			if(count<goal) {
+		        		break;
+		        	}
+		        	width--;
+		        	j++;
+		        	i--;
+		        	
+		        	
+		        	for(int a=0; a<height; a++) {
+		        			arr[i][j] = count;
+		        			i--;
+		        			count--;
+		        			if(count<goal) {
+				        		break;
+				        	}
+		        	}
+		        	height--;
+		        	i++;
+		        	j++;
+		        	
+		        	
+		        	for(int a=0; a<width; a++) {
+		        		arr[i][j] = count;
+		        		j++;
+		        		count--;	  
+		        		if(count<goal) {
+			        		break;
+			        	}
+		        	}
+		        	width--;
+		        	j--;
+		        	i++;
+		        	
+		        }
+		        
+		        for(int a=0; a<n; a++) {
+		        	for(int b=0; b<m; b++) {
+		        		System.out.printf(arr[a][b] + " ");
+		        	}
+		        	System.out.println();
+		        }
+		        
+		        scan.close();
+
+	}
+
+}
