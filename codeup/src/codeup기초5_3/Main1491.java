cdad74291e9efd2cae4c5cb46ff3b9282a3d5214
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-25 19:04:05 +0900

    코드업 1491번 풀이

---
+++ codeup/src/codeup기초5_3/Main1491.java
@@ -0,0 +1,88 @@
+package codeup기초5_3;
+
+import java.util.Scanner;
+
+public class Main1491 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		int m = scan.nextInt();
+		 int[][] arr = new int[n][m];
+	        
+	        int count = n*m;
+	        int goal = 1;
+	        int i = n-1;
+	        int j = 0;
+	        int width = m-1;
+	        int height = n;
+	        
+	        
+	        while(count>=goal) {
+	        	
+	        	//왼쪽
+	        	for(int a=0; a<height; a++) {
+	        		arr[i][j] = count;
+	        		i--;
+	        		count--;
+	        	}
+	        	height--;
+	        	i++;
+	        	j++;
+	        	if(count<goal) {
+	        		break;
+	        	}
+	        	
+	        	//위
+	        	for(int a=0; a<width; a++) {	        		
+	        		arr[i][j] = count;	        		
+	        		j++;
+	        		count--;
+	        	}
+	        	width--;
+	        	j--;
+	        	i++;
+	        	if(count<goal) {
+	        		break;
+	        	}
+	        	
+	        	//오른
+	        	for(int a=0; a<height; a++) {	        		
+	        		arr[i][j] = count;	        		
+	        		i++;
+	        		count--;
+	        	}
+	        	height--;
+	        	i--;
+	        	j--;
+	        	if(count<goal) {
+	        		break;
+	        	}
+	        	
+	        	//아래
+	        	for(int a=0; a<width; a++) {
+	        		arr[i][j] = count;
+	        		j--;
+	        		count--;
+	        	}
+	        	width--;
+	        	j++;
+	        	i--;
+	        	if(count<goal) {
+	        		break;
+	        	}
+	        	
+	        }
+	        
+	        for(int a=0; a<n; a++) {
+	        	for(int b=0; b<m; b++) {
+	        		System.out.printf(arr[a][b] + " ");
+	        	}
+	        	System.out.println();
+	        }
+	        
+	        scan.close();
+	}
+}
