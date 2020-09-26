9cf35c8351a18e8fc66adf60117bdb43c3631b8c
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-09-26 16:40:59 +0900

    코드업 3120번 풀이

---
+++ codeup/src/그리디/Main3120.java
@@ -0,0 +1,32 @@
+package ±×¸®µð;
+
+import java.util.Scanner;
+
+public class Main3120 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		int[] array = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
+		int index = 0;
+		int answer = 0;
+		
+		while(n!=0) {
+			int change = n / array[index];
+			
+			n = n - (change*array[index++]);
+			
+			answer = answer + change;
+			
+		}
+			
+		System.out.println(answer);
+	
+		scan.close();
+		
+	}
+
+}
