b37ff985473601b220cde6d724acd4b3f950e59a
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-10-01 20:45:08 +0900

    코드업 3007번 풀이

---
+++ codeup/src/관리자추천문제/Main3007.java
@@ -0,0 +1,42 @@
+package 관리자추천문제;
+
+import java.util.Scanner;
+
+public class Main3007 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		int m = scan.nextInt();
+		
+		int[] arr = new int[n];
+		for(int i=0; i<n; i++) {
+			arr[i] = scan.nextInt();
+		}
+		
+		int[][] arr2 = new int[m][2];
+		for(int i=0; i<m; i++) {
+			for(int j=0; j<2; j++) {
+				arr2[i][j] = scan.nextInt();
+			}
+		}
+		
+		int count = 0;
+		int sum = 0;
+		
+		while(count<m) {
+		for(int i=(arr2[count][0])-1; i<=(arr2[count][1])-1; i++) {
+			sum = sum + arr[i];
+		}
+		
+		System.out.println(sum);
+		sum = 0;
+			count++;
+		}
+
+		scan.close();
+	}
+
+}
